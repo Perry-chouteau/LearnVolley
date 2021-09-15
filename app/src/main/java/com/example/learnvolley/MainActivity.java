c3928429fe3bbbfd0298c852f1be0410df1c9435
perry chouteau <perry.chouteau@epitech.eu>
2021-09-15 20:18:08 +0200

    [EDIT] : Simplify code (Refactor & transform to lambda)

--- app/src/main/java/com/example/learnvolley/MainActivity.java
+++ app/src/main/java/com/example/learnvolley/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.learnvolley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -30,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Init
         // Instantiate the RequestQueue.
-        tmp = findViewById(R.id.editTextUrl);
+        tmp = findViewById(R.id.editTextURL);
         textView = (TextView) findViewById(R.id.textView);
         queue = Volley.newRequestQueue(this);
         button = findViewById(R.id.button);
@@ -40,18 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 url = tmp.getText().toString(); // URL OF DATA
                 // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest( Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                textView.setText(response);
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                textView.setText("This URL doesn't exist!");
-                            }
-                        }
+                        response -> textView.setText(response),
+                        error -> textView.setText("This URL doesn't exist!")
                 );
 
                 // Add the request to the RequestQueue.
