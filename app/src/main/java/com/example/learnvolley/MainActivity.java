f541712b6124a55a13c37405c8311bb354f3596b
perry chouteau <perry.chouteau@epitech.eu>
2021-09-16 10:38:22 +0200

    [EDIT] : reset local files from an old commit to make it work again and update it to the latest version.

--- app/src/main/java/com/example/learnvolley/MainActivity.java
+++ app/src/main/java/com/example/learnvolley/MainActivity.java
@@ -9,16 +9,18 @@ import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    RequestQueue queue;
-    String url;
     TextView tmp;
+    TextView textView;
     Button button;
+    String url;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Init
-        // Instantiate the RequestQueue.
         tmp = findViewById(R.id.editText);
-        textView = (TextView) findViewById(R.id.textView);
-        queue = Volley.newRequestQueue(this);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        // Instantiate the RequestQueue.
+        queue = Volley.newRequestQueue(this);
 
         button.setOnClickListener( (View view) -> {
-
-                url = tmp.getText().toString(); // URL OF DATA
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest( Request.Method.GET, url,
-                        response -> textView.setText(response),
-                        error -> textView.setText("This URL doesn't exist!")
-                );
-
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);
+            url = tmp.getText().toString();
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest( Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display characters of the response string.
+                        textView.setHint(getText(R.string.textViewHint));
+                        textView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle Url Error.
+                        if (url != null && url.length() != 0) {
+                            textView.setText("");
+                            textView.setHint(getText(R.string.textViewBadUrl_p1) + url + getText(R.string.textViewBadUrl_p2));
+                        } else {
+                            textView.setText("");
+                            textView.setHint(getText(R.string.textViewNoUrl));
+                        }
+                    }
+                }
+            );
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
         });
     }
 }
